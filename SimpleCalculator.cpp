bfbed611a362932100f2429d7ad84a64352e148a
Phạm Nguyễn Thu Giang <24022784@vnu.edu.vn>
2025-02-26 22:14:09 +0700

    update simple calculator

---
+++ SimpleCalculator.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int arithmetic (int num1, int num2, char op){
+        switch (op){
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case 'x':
+            return num1 * num2;
+        case '/':
+            if (num2 == 0){
+                cout << "Invalid divisor" << endl;
+                exit(1);
+            }
+            else return num1 / num2;
+        case '%':
+            if (num2 == 0){
+                cout << "Invalid divisor" << endl;
+                exit(1);
+            }
+            else return num1 / num2;
+        default:
+            cout << "Invalid operator" << endl;
+            exit(1);
+        }
+    }
+int main(int argc, char* argv[]){
+
+    int num1, num2;
+    char op;
+
+    num1 = atoi(argv[1]);
+    op = argv[2][0];
+    num2 = atoi(argv[3]);
+    arithmetic(num1, num2, op);
+    return 0;
+
+}
